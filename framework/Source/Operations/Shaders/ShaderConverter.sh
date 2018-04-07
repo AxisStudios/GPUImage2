a1422ce79f2df45f0c84c6c78bd81620c691243f
Premysl Vlcek <premysl.vlcek@strv.com>
2018-04-08 00:50:33 +0200

    pretty printed shaders

--- framework/Source/Operations/Shaders/ShaderConverter.sh
+++ framework/Source/Operations/Shaders/ShaderConverter.sh
@@ -4,9 +4,9 @@
 import Foundation
 
 enum OpenGLPlatform {
-    case OpenGL
-    case OpenGLES
-    case Both
+case OpenGL
+case OpenGLES
+case Both
 }
 
 let fileNames = CommandLine.arguments.dropFirst()
@@ -15,49 +15,49 @@ var allConvertedGLShaders = ""
 var allConvertedGLESShaders = ""
 
 for fileName in fileNames {
-    let pathURL = URL(fileURLWithPath:fileName)
-    let pathExtension = pathURL.pathExtension
-    let baseName = pathURL.deletingPathExtension().lastPathComponent
-    
-    guard (FileManager.default.fileExists(atPath:pathURL.path)) else {
-        print("Error: file \"\(fileName)\" could not be found.")
-        continue
-    }
-    
-    let shaderSuffix:String
-    if (pathExtension.lowercased() == "vsh") {
-        shaderSuffix = "VertexShader"
-    } else if (pathExtension.lowercased() == "fsh") {
-        shaderSuffix = "FragmentShader"
-    } else {
-        continue
-    }
-    
-    let convertedShaderName:String
-    let shaderPlatform:OpenGLPlatform
-    if baseName.hasSuffix("_GLES") {
-        convertedShaderName = "\(baseName.replacingOccurrences(of:"_GLES", with:""))\(shaderSuffix)"
-        shaderPlatform = .OpenGLES
-    } else if baseName.hasSuffix("_GL") {
-        convertedShaderName = "\(baseName.replacingOccurrences(of:"_GL", with:""))\(shaderSuffix)"
-        shaderPlatform = .OpenGL
-    } else {
-        convertedShaderName = "\(baseName)\(shaderSuffix)"
-        shaderPlatform = .Both
-    }
-    
-    var accumulatedString = "public let \(convertedShaderName) = \"\"\"\n"
-    let fileContents = try String(contentsOfFile:fileName, encoding:String.Encoding.ascii)
-    accumulatedString += fileContents
-    accumulatedString += "\n\"\"\"\n"
-    
-    switch (shaderPlatform) {
-    case .OpenGL: allConvertedGLShaders += accumulatedString
-    case .OpenGLES: allConvertedGLESShaders += accumulatedString
-    case .Both:
-        allConvertedGLShaders += accumulatedString
-        allConvertedGLESShaders += accumulatedString
-    }
+let pathURL = URL(fileURLWithPath:fileName)
+let pathExtension = pathURL.pathExtension
+let baseName = pathURL.deletingPathExtension().lastPathComponent
+
+guard (FileManager.default.fileExists(atPath:pathURL.path)) else {
+print("Error: file \"\(fileName)\" could not be found.")
+continue
+}
+
+let shaderSuffix:String
+if (pathExtension.lowercased() == "vsh") {
+shaderSuffix = "VertexShader"
+} else if (pathExtension.lowercased() == "fsh") {
+shaderSuffix = "FragmentShader"
+} else {
+continue
+}
+
+let convertedShaderName:String
+let shaderPlatform:OpenGLPlatform
+if baseName.hasSuffix("_GLES") {
+convertedShaderName = "\(baseName.replacingOccurrences(of:"_GLES", with:""))\(shaderSuffix)"
+shaderPlatform = .OpenGLES
+} else if baseName.hasSuffix("_GL") {
+convertedShaderName = "\(baseName.replacingOccurrences(of:"_GL", with:""))\(shaderSuffix)"
+shaderPlatform = .OpenGL
+} else {
+convertedShaderName = "\(baseName)\(shaderSuffix)"
+shaderPlatform = .Both
+}
+
+var accumulatedString = "public let \(convertedShaderName) = \"\"\"\n"
+let fileContents = try String(contentsOfFile:fileName, encoding:String.Encoding.ascii)
+accumulatedString += fileContents
+accumulatedString += "\n\"\"\"\n"
+
+switch (shaderPlatform) {
+case .OpenGL: allConvertedGLShaders += accumulatedString
+case .OpenGLES: allConvertedGLESShaders += accumulatedString
+case .Both:
+allConvertedGLShaders += accumulatedString
+allConvertedGLESShaders += accumulatedString
+}
 }
 
 let scriptURL = URL(fileURLWithPath:CommandLine.arguments.first!)
