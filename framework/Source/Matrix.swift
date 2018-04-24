3942e27b8f1518c10a43a2dd432a7121cf5050c9
Premysl Vlcek <premysl.vlcek@strv.com>
2018-04-24 22:23:36 +0200

    Added some utilities for Matrix3x3

--- framework/Source/Matrix.swift
+++ framework/Source/Matrix.swift
@@ -142,24 +142,42 @@ public struct Matrix4x4 {
 }
 
 public struct Matrix3x3 {
-    public var m11:Float, m12:Float, m13:Float
-    public var m21:Float, m22:Float, m23:Float
-    public var m31:Float, m32:Float, m33:Float
+    public var m11:Float = 0, m12:Float = 0, m13:Float = 0
+    public var m21:Float = 0, m22:Float = 0, m23:Float = 0
+    public var m31:Float = 0, m32:Float = 0, m33:Float = 0
+
+    public var rowMajorValues: [Float] {
+        get {
+            return [
+                m11, m12, m13,
+                m21, m22, m23,
+                m31, m32, m33
+            ]
+        } set {
+            loadVariablesFromRowMajorValeues(rowMajorValues: newValue)
+        }
+    }
+
+    private mutating func loadVariablesFromRowMajorValeues(rowMajorValues: [Float]) {
+        guard rowMajorValues.count > 8 else { fatalError("Tried to initialize a 4x4 matrix with fewer than 9 values") }
+
+        self.m11 = rowMajorValues[0]
+        self.m12 = rowMajorValues[1]
+        self.m13 = rowMajorValues[2]
+
+        self.m21 = rowMajorValues[4]
+        self.m22 = rowMajorValues[5]
+        self.m23 = rowMajorValues[6]
+
+        self.m31 = rowMajorValues[8]
+        self.m32 = rowMajorValues[9]
+        self.m33 = rowMajorValues[10]
+    }
     
     public init(rowMajorValues:[Float]) {
         guard rowMajorValues.count > 8 else { fatalError("Tried to initialize a 3x3 matrix with fewer than 9 values") }
         
-        self.m11 = rowMajorValues[0]
-        self.m12 = rowMajorValues[1]
-        self.m13 = rowMajorValues[2]
-        
-        self.m21 = rowMajorValues[3]
-        self.m22 = rowMajorValues[4]
-        self.m23 = rowMajorValues[5]
-        
-        self.m31 = rowMajorValues[6]
-        self.m32 = rowMajorValues[7]
-        self.m33 = rowMajorValues[8]
+        loadVariablesFromRowMajorValeues(rowMajorValues: rowMajorValues)
     }
     
     public static let identity = Matrix3x3(rowMajorValues:[1.0, 0.0, 0.0,
@@ -169,6 +187,20 @@ public struct Matrix3x3 {
     public static let centerOnly = Matrix3x3(rowMajorValues:[0.0, 0.0, 0.0,
                                                              0.0, 1.0, 0.0,
                                                              0.0, 0.0, 0.0])
+
+    public subscript(row: Int, column: Int) -> Float {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return rowMajorValues[(row * 3) + column]
+        } set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            rowMajorValues[(row * 3) + column] = newValue
+        }
+    }
+
+    private func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < 3 && column >= 0 && column < 3
+    }
 }
 
 #if !os(Linux)
