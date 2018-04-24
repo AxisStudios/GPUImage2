4c9753bf907239554292d152220747a61e93d13c
Premysl Vlcek <premysl.vlcek@strv.com>
2018-04-24 23:17:20 +0200

    Some matrix operations fixed

--- framework/Source/Matrix.swift
+++ framework/Source/Matrix.swift
@@ -64,7 +64,7 @@ public struct Matrix4x4 {
     public init() {
 
     }
-    
+
     public init(rowMajorValues:[Float]) {
         loadVariablesFromRowMajorValeues(rowMajorValues: rowMajorValues)
     }
@@ -92,7 +92,7 @@ public struct Matrix4x4 {
         self.m43 = rowMajorValues[14]
         self.m44 = rowMajorValues[15]
     }
-    
+
     public static let identity = Matrix4x4(rowMajorValues:[1.0, 0.0, 0.0, 0.0,
                                                            0.0, 1.0, 0.0, 0.0,
                                                            0.0, 0.0, 1.0, 0.0,
@@ -109,6 +109,14 @@ public struct Matrix4x4 {
         } set {
             assert(indexIsValid(row: row, column: column), "Index out of range")
             rowMajorValues[(row * 4) + column] = newValue
+        }
+    }
+
+    public subscript(index: Int) -> Float {
+        get {
+            return rowMajorValues[index]
+        } set {
+            rowMajorValues[index] = newValue
         }
     }
 
@@ -165,25 +173,25 @@ public struct Matrix3x3 {
         self.m12 = rowMajorValues[1]
         self.m13 = rowMajorValues[2]
 
-        self.m21 = rowMajorValues[4]
-        self.m22 = rowMajorValues[5]
-        self.m23 = rowMajorValues[6]
+        self.m21 = rowMajorValues[3]
+        self.m22 = rowMajorValues[4]
+        self.m23 = rowMajorValues[5]
 
-        self.m31 = rowMajorValues[8]
-        self.m32 = rowMajorValues[9]
-        self.m33 = rowMajorValues[10]
+        self.m31 = rowMajorValues[6]
+        self.m32 = rowMajorValues[7]
+        self.m33 = rowMajorValues[8]
     }
-    
+
     public init(rowMajorValues:[Float]) {
         guard rowMajorValues.count > 8 else { fatalError("Tried to initialize a 3x3 matrix with fewer than 9 values") }
-        
+
         loadVariablesFromRowMajorValeues(rowMajorValues: rowMajorValues)
     }
-    
+
     public static let identity = Matrix3x3(rowMajorValues:[1.0, 0.0, 0.0,
                                                            0.0, 1.0, 0.0,
                                                            0.0, 0.0, 1.0])
-    
+
     public static let centerOnly = Matrix3x3(rowMajorValues:[0.0, 0.0, 0.0,
                                                              0.0, 1.0, 0.0,
                                                              0.0, 0.0, 0.0])
@@ -195,6 +203,14 @@ public struct Matrix3x3 {
         } set {
             assert(indexIsValid(row: row, column: column), "Index out of range")
             rowMajorValues[(row * 3) + column] = newValue
+        }
+    }
+
+    public subscript(index: Int) -> Float {
+        get {
+            return rowMajorValues[index]
+        } set {
+            rowMajorValues[index] = newValue
         }
     }
 
@@ -210,23 +226,23 @@ public extension Matrix4x4 {
         self.m12 = Float(transform3D.m12)
         self.m13 = Float(transform3D.m13)
         self.m14 = Float(transform3D.m14)
-        
+
         self.m21 = Float(transform3D.m21)
         self.m22 = Float(transform3D.m22)
         self.m23 = Float(transform3D.m23)
         self.m24 = Float(transform3D.m24)
-        
+
         self.m31 = Float(transform3D.m31)
         self.m32 = Float(transform3D.m32)
         self.m33 = Float(transform3D.m33)
         self.m34 = Float(transform3D.m34)
-        
+
         self.m41 = Float(transform3D.m41)
         self.m42 = Float(transform3D.m42)
         self.m43 = Float(transform3D.m43)
         self.m44 = Float(transform3D.m44)
     }
-    
+
     public init (_ transform:CGAffineTransform) {
         self.init(CATransform3DMakeAffineTransform(transform))
     }
@@ -249,10 +265,10 @@ public extension Matrix4x4 {
         }
 
         self.init(rowMajorValues:[
-        scale / r_l, 0.0, 0.0, tx,
-        0.0, scale / t_b, 0.0, ty,
-        0.0, 0.0, scale / f_n, tz,
-        0.0, 0.0, 0.0, 1.0])
+            scale / r_l, 0.0, 0.0, tx,
+            0.0, scale / t_b, 0.0, ty,
+            0.0, 0.0, scale / f_n, tz,
+            0.0, 0.0, 0.0, 1.0])
     }
 }
 #endif
